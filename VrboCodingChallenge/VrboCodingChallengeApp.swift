d105fff30ba3163c6e2ad603a65dd91b8fcb5d3a
Matthew Waller <matthew@blueshiftdevelopment.com>
2020-10-22 11:10:52 -0500

    Initial Commit

---
+++ VrboCodingChallenge/VrboCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VrboCodingChallengeApp.swift
+//  VrboCodingChallenge
+//
+//  Created by Matthew Waller on 10/22/20.
+//
+
+import SwiftUI
+
+@main
+struct VrboCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
